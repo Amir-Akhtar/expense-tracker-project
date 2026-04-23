8b3fb61c24ddd7a32a0579479789b3805cdd910f
Amir Akhtar <imbin.akhtar@gmail.com>
2026-04-23 14:20:44 +0000

    added jenkinsfile

--- expense-tracker-ui/src/environments/environment.prod.ts
+++ expense-tracker-ui/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: https://expense-tracker-project-1x0f.onrender.com/api/v1/expense'
+  baseUrl: 'https://expense-tracker-project-1x0f.onrender.com/api/v1/expense'
 };
